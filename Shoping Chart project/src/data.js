a2b7acff1f01e8d73b8a15fa69a70e72523574cd
imacul77 <122232213+imacul77@users.noreply.github.com>
2023-02-25 23:37:07 +0100

    Add files via upload

---
+++ Shoping Chart project/src/data.js
@@ -0,0 +1,31 @@
+let shopItemsData = [{
+    id: "data_1",
+    name: "Brown bags",
+    price: "45",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit!",
+    img: "images/bags.jpg"
+}, {
+    id: "data_2",
+    name: " sneakers",
+    price: "100",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit!",
+    img: "images/Sneakers.jpg"
+}, {
+    id: "data_3",
+    name: "Designer Shirt",
+    price: "200",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit!",
+    img: "images/designerShirt.jpg"
+}, {
+    id: "data_4",
+    name: "clasic jacket",
+    price: "300",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit!",
+    img: "images/Jacket.jpg"
+}, {
+    id: "data_5",
+    name: "clasic shirt",
+    price: "300",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit!",
+    img: "images/shirt.png"
+}];
